a620e931cc5a6d7225b87b6c91d7541f3e8baaa0
Tejas J Shetty <56498313+Tejas1546@users.noreply.github.com>
2026-03-13 17:26:30 +0530

    Implimented task 1-3 (#9)
    * Implimented task 1-3
    
    * Implimented vitest task 4 (#12)
    
    Co-authored-by: tejas <tejas.shetty@codecraft.co.in>
    
    * replimented as per the review changes
    
    ---------
    
    Co-authored-by: tejas <tejas.shetty@codecraft.co.in>

---
+++ Assignment 3/task 1.ts
@@ -0,0 +1,110 @@
+import { stat, readdir } from "fs";
+import pathMod from "path";
+import { promises as fsPromises } from "fs";
+
+/**
+ * the getFileType() is a simple function that returns the path type like "FILE" | "DIRECTORY" | "OTHER". This uses promise based callback system using fs.stat based wrapper wrapping is manually
+ * @param path takes in the absolute path of a file or a folder
+ * @returns the path is a File or a folder
+ */
+function getFileType(path: string): Promise<"FILE" | "DIRECTORY" | "OTHER"> {
+  return new Promise((resolve, reject) => {
+    stat(path, (err, stats) => {
+      if (err) {
+        return reject(new Error("file system error"));
+      }
+      if (stats.isFile()) return resolve("FILE");
+      if (stats.isDirectory()) return resolve("DIRECTORY");
+
+      return resolve("OTHER");
+    });
+  });
+}
+
+/**
+ * the getContens() returns the folder contents or the path of the file. this function also uses the promise based callback system that uses fs.stat for wrapping the function
+ * @param path takes in the absolute path of a file or a folder
+ * @returns if the path is a file then it returns the path, if it is directory it acts like the 'ls' command used in linux systems
+ */
+const getContents = (path: string): Promise<string | string[]> => {
+  return getFileType(path)
+    .then((file) => {
+      if (file === "FILE") {
+        return [path];
+      }
+      if (file === "DIRECTORY") {
+        return fsPromises.readdir(path);
+      }
+      return path;
+    })
+    .catch((err) => {
+      throw new Error("File system error: " + err.message);
+    });
+};
+
+/**
+ * the getSize() returns the size of the file or the folder of the given path in bytes.this function also uses the promise based callback system that uses fs.stat for wrapping the function
+ * @param path takes in the absolute path of a file or a folder
+ * @returns returns the size of the file or the folder of the given path in bytes
+ */
+function getSize(path: string): Promise<number> {
+  return new Promise((resolve, reject) => {
+    stat(path, (err, stats) => {
+      if (err) {
+        return reject(new Error("file system error"));
+      }
+
+      if (stats.isFile()) {
+        return resolve(stats.size);
+      }
+
+      if (stats.isDirectory()) {
+        readdir(path, (err, files) => {
+          if (err) return reject(new Error("file system error"));
+
+          function processNextFile(
+            index: number,
+            accumulatedSize: number,
+          ): void {
+            if (index >= files.length) {
+              return resolve(accumulatedSize);
+            }
+
+            const fullPath = pathMod.join(path, files[index]);
+
+            getSize(fullPath)
+              .then((size) => {
+                processNextFile(index + 1, accumulatedSize + size);
+              })
+              .catch(() => {
+                reject(new Error("file system error"));
+              });
+          }
+
+          processNextFile(0, 0);
+        });
+      } else {
+        resolve(0);
+      }
+    });
+  });
+}
+
+//console based testing
+const tpath =
+  "/mnt/c/Users/Tejas/Desktop/internship/task/cc-7-Tejas/Assignment 3/task 1.ts";
+getFileType(tpath)
+  .then((type) => {
+    console.log("Type:", type);
+    return getContents(tpath);
+  })
+  .then((contents) => {
+    console.log(contents);
+    return getSize(tpath);
+  })
+  .then((size) => {
+    console.log("Total Size: ", size);
+  })
+  .catch((err) => {
+    console.error("Error:", err.message);
+  });
---
+++ Assignment 3/task 3.ts
@@ -0,0 +1,90 @@
+import { stat, readdir } from "fs/promises";
+import pathM from "path";
+
+/**
+ * the getFileType() returns the path type like "FILE" | "DIRECTORY" | "OTHER". Here we use the async/await with try and catch.
+ * @param path takes in the absolute path of a file or a folder
+ * @returns the path is a File or a folder
+ */
+export async function getFileType(
+  path: string,
+): Promise<"FILE" | "DIRECTORY" | "OTHER"> {
+  try {
+    const stats = await stat(path);
+    if (stats.isFile()) return "FILE";
+    if (stats.isDirectory()) return "DIRECTORY";
+    return "OTHER";
+  } catch (error) {
+    throw new Error("file system error", { cause: error });
+  }
+}
+
+/**
+ * the getContens() returns the folder contents or the path of the file. Here we use the async/await with try and catch.
+ * @param path takes in the absolute path of a file or a folder
+ * @returns if the path is a file then it returns the path, if it is directory it acts like the 'ls' command used in linux systems
+ */
+export async function getContents(path: string): Promise<string | string[]> {
+  try {
+    const type = await getFileType(path);
+    if (type === "FILE") return path;
+    if (type === "DIRECTORY") {
+      const files = await readdir(path);
+      return files;
+    }
+    return path;
+  } catch (error) {
+    throw new Error("file system error", { cause: error });
+  }
+}
+
+/**
+ * the getSize() returns the size of the file or the folder of the given path in bytes.Here we use the async/await with try and catch.
+ * @param path takes in the absolute path of a file or a folder
+ * @returns returns the size of the file or the folder of the given path in bytes
+ */
+export async function getSize(path: string): Promise<number> {
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    throw new Error("file system error", { cause: error });
+  }
+  if (stats.isFile()) {
+    return stats.size;
+  }
+  if (stats.isDirectory()) {
+    try {
+      const files = await readdir(path);
+      let tSize = 0;
+      for (const file of files) {
+        const fpath = pathM.join(path, file);
+        tSize += await getSize(fpath);
+      }
+
+      return tSize;
+    } catch (error) {
+      throw new Error("file system error", { cause: error });
+    }
+  }
+  return 0;
+}
+
+//console based testing
+const tpath =
+  "/mnt/c/Users/Tejas/Desktop/internship/task/cc-7-Tejas/Assignment 3/task 1.ts";
+getFileType(tpath)
+  .then((type) => {
+    console.log("Type:", type);
+    return getContents(tpath);
+  })
+  .then((contents) => {
+    console.log(contents);
+    return getSize(tpath);
+  })
+  .then((size) => {
+    console.log("Total Size: ", size);
+  })
+  .catch((err) => {
+    console.error("Error:", err.message);
+  });
