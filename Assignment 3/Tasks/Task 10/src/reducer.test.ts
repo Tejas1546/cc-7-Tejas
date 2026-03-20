2eb7f85f9896bdd2cf938bd2deac58298e5e0d95
tejas <jshettytejas@gmail.com>
2026-03-20 11:27:56 +0000

    fixes and implimented unit testing

--- Assignment 3/Tasks/Task 10/src/reducer.test.ts
+++ Assignment 3/Tasks/Task 10/src/reducer.test.ts
@@ -6,6 +6,7 @@ import {
   type Action,
 } from './reducer';
 
+<<<<<<< HEAD
 describe('Mode: normal', () => {
   it('Start recording', () => {
     const action: Action = { type: 'START_RECORDING', timestamp: 1000 };
@@ -21,10 +22,25 @@ describe('Mode: normal', () => {
   it('Start playback', () => {
     const stateWithEmptyRecording: ApplicationState = {
       ...initialState,
+=======
+describe('Drumkit reducer module test', () => {
+  it('Start recording and reset the old recording track', () => {
+    const action: Action = { type: 'START_RECORDING', timestamp: 1000 };
+    const nextState = reducer(initialState, action);
+    expect(nextState.mode).toBe('recording-progress');
+    expect(nextState.recordings).not.toBeNull();
+    expect(nextState.recordings?.beats).toEqual([]);
+  });
+
+  it('ignore playback if no beats in store', () => {
+    const stateWithEmptyRecording: ApplicationState = {
+      mode: 'normal',
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
       recordings: { beats: [] },
     };
     const action: Action = { type: 'START_PLAYBACK' };
     const nextState = reducer(stateWithEmptyRecording, action);
+<<<<<<< HEAD
 
     expect(nextState.mode).toBe('normal');
   });
@@ -42,10 +58,21 @@ describe('Mode: normal', () => {
   });
 
   it('Ignores invalid actions', () => {
+=======
+    expect(nextState.mode).toBe('normal');
+  });
+
+  it('have to append the beats in recording-progress mode', () => {
+    const activeState: ApplicationState = {
+      mode: 'recording-progress',
+      recordings: { beats: [] },
+    };
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
     const action: Action = {
       type: 'BEAT',
       data: { key: 'KeyA', timestamp: 1500 },
     };
+<<<<<<< HEAD
     const nextState = reducer(initialState, action);
     expect(nextState).toBe(initialState);
   });
@@ -182,5 +209,62 @@ describe('Playback Flow', () => {
 
     expect(nextState.mode).toBe('normal');
     expect(nextState.playbackOffset).toBe(0);
+=======
+    const nextState = reducer(activeState, action);
+    expect(nextState.recordings?.beats.length).toBe(1);
+    expect(nextState.recordings?.beats[0]).toEqual({
+      key: 'KeyA',
+      timestamp: 1500,
+    });
+  });
+
+  it('ignore beats if not in recording-progress mode', () => {
+    const normalState: ApplicationState = {
+      mode: 'normal',
+      recordings: { beats: [] },
+    };
+    const action: Action = {
+      type: 'BEAT',
+      data: { key: 'KeyA', timestamp: 1500 },
+    };
+    const nextState = reducer(normalState, action);
+    expect(nextState.recordings?.beats.length).toBe(0);
+  });
+
+  it('pause resume and stop recording test', () => {
+    let state: ApplicationState = {
+      mode: 'recording-progress',
+      recordings: { beats: [{ key: 'KeyA', timestamp: 100 }] },
+    };
+    state = reducer(state, { type: 'PAUSE_RECORDING', timestamp: 2000 });
+    expect(state.mode).toBe('recording-paused');
+    state = reducer(state, { type: 'CONTINUE_RECORDING', timestamp: 3000 });
+    expect(state.mode).toBe('recording-progress');
+    state = reducer(state, { type: 'STOP_RECORDING' });
+    expect(state.mode).toBe('normal');
+    expect(state.recordings?.beats.length).toBe(1);
+  });
+
+  it('start playback if recording exists', () => {
+    const validTrackState: ApplicationState = {
+      mode: 'normal',
+      recordings: { beats: [{ key: 'KeyA', timestamp: 100 }] },
+    };
+    const nextState = reducer(validTrackState, { type: 'START_PLAYBACK' });
+    expect(nextState.mode).toBe('playback-progress');
+  });
+
+  it('the playback has to work through pause,continue, and stop playback', () => {
+    let state: ApplicationState = {
+      mode: 'playback-progress',
+      recordings: { beats: [{ key: 'KeyA', timestamp: 100 }] },
+    };
+    state = reducer(state, { type: 'PAUSE_PLAYBACK', timestamp: 2000 });
+    expect(state.mode).toBe('playback-paused');
+    state = reducer(state, { type: 'CONTINUE_PLAYBACK' });
+    expect(state.mode).toBe('playback-progress');
+    state = reducer(state, { type: 'STOP_PLAYBACK' });
+    expect(state.mode).toBe('normal');
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
   });
 });
--- Assignment 3/Tasks/Task 10/src/reducer.ts
+++ Assignment 3/Tasks/Task 10/src/reducer.ts
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 // --- TYPES ---
 
+=======
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
 export type Beat = {
   key: string;
   timestamp: number;
@@ -17,6 +20,7 @@ export type ApplicationState = {
     | 'playback-progress'
     | 'playback-paused';
   recordings: Recording | null;
+<<<<<<< HEAD
   recordingStartTime: number;
   recordingOffset: number;
   playbackOffset: number;
@@ -41,6 +45,42 @@ type PausePlaybackAction = {
 };
 type StopPlaybackAction = { type: 'STOP_PLAYBACK' };
 type ContinuePlaybackAction = { type: 'CONTINUE_PLAYBACK' };
+=======
+};
+
+type StartRecordingAction = {
+  type: 'START_RECORDING';
+  timestamp: number;
+};
+type StartPlaybackAction = {
+  type: 'START_PLAYBACK';
+};
+type ContinueRecordingAction = {
+  type: 'CONTINUE_RECORDING';
+  timestamp: number;
+};
+type StopRecordingAction = {
+  type: 'STOP_RECORDING';
+};
+type PauseRecordingAction = {
+  type: 'PAUSE_RECORDING';
+  timestamp: number;
+};
+type BeatAction = {
+  type: 'BEAT';
+  data: Beat;
+};
+type PausePlaybackAction = {
+  type: 'PAUSE_PLAYBACK';
+  timestamp: number;
+};
+type StopPlaybackAction = {
+  type: 'STOP_PLAYBACK';
+};
+type ContinuePlaybackAction = {
+  type: 'CONTINUE_PLAYBACK';
+};
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
 
 export type Action =
   | StartRecordingAction
@@ -56,9 +96,12 @@ export type Action =
 export const initialState: ApplicationState = {
   mode: 'normal',
   recordings: null,
+<<<<<<< HEAD
   recordingStartTime: 0,
   recordingOffset: 0,
   playbackOffset: 0,
+=======
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
 };
 
 export const reducer = (
@@ -68,6 +111,7 @@ export const reducer = (
   switch (state.mode) {
     case 'normal':
       switch (action.type) {
+<<<<<<< HEAD
         case 'START_RECORDING':
           return {
             mode: 'recording-progress',
@@ -84,6 +128,26 @@ export const reducer = (
             mode: 'playback-progress',
             playbackOffset: 0,
           };
+=======
+        case 'START_RECORDING': {
+          const newRecording: Recording = {
+            beats: [],
+          };
+          return {
+            mode: 'recording-progress',
+            recordings: newRecording,
+          };
+        }
+        case 'START_PLAYBACK': {
+          if (!state.recordings || state.recordings.beats.length === 0) {
+            return state;
+          }
+          return {
+            mode: 'playback-progress',
+            recordings: state.recordings,
+          };
+        }
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
         default:
           return state;
       }
@@ -92,6 +156,7 @@ export const reducer = (
       switch (action.type) {
         case 'PAUSE_RECORDING':
           return {
+<<<<<<< HEAD
             ...state,
             mode: 'recording-paused',
             recordingOffset: action.currentOffset,
@@ -111,6 +176,28 @@ export const reducer = (
                 ? action.newStartTime
                 : state.recordingStartTime,
           };
+=======
+            mode: 'recording-paused',
+            recordings: state.recordings,
+          };
+        case 'STOP_RECORDING':
+          return {
+            mode: 'normal',
+            recordings: state.recordings,
+          };
+        case 'BEAT': {
+          if (!state.recordings) return state;
+
+          const updatedRecording: Recording = {
+            beats: state.recordings.beats.concat(action.data),
+          };
+
+          return {
+            mode: state.mode,
+            recordings: updatedRecording,
+          };
+        }
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
         default:
           return state;
       }
@@ -119,12 +206,23 @@ export const reducer = (
       switch (action.type) {
         case 'CONTINUE_RECORDING':
           return {
+<<<<<<< HEAD
             ...state,
             mode: 'recording-progress',
             recordingStartTime: action.newStartTime,
           };
         case 'STOP_RECORDING':
           return { ...state, mode: 'normal' };
+=======
+            mode: 'recording-progress',
+            recordings: state.recordings,
+          };
+        case 'STOP_RECORDING':
+          return {
+            mode: 'normal',
+            recordings: state.recordings,
+          };
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
         default:
           return state;
       }
@@ -133,6 +231,7 @@ export const reducer = (
       switch (action.type) {
         case 'PAUSE_PLAYBACK':
           return {
+<<<<<<< HEAD
             ...state,
             mode: 'playback-paused',
             playbackOffset: action.currentPlaybackTime,
@@ -142,6 +241,15 @@ export const reducer = (
             ...state,
             mode: 'normal',
             playbackOffset: 0,
+=======
+            mode: 'playback-paused',
+            recordings: state.recordings,
+          };
+        case 'STOP_PLAYBACK':
+          return {
+            mode: 'normal',
+            recordings: state.recordings,
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
           };
         default:
           return state;
@@ -151,6 +259,7 @@ export const reducer = (
       switch (action.type) {
         case 'CONTINUE_PLAYBACK':
           return {
+<<<<<<< HEAD
             ...state,
             mode: 'playback-progress',
           };
@@ -159,16 +268,32 @@ export const reducer = (
             ...state,
             mode: 'normal',
             playbackOffset: 0,
+=======
+            mode: 'playback-progress',
+            recordings: state.recordings,
+          };
+        case 'STOP_PLAYBACK':
+          return {
+            mode: 'normal',
+            recordings: state.recordings,
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
           };
         default:
           return state;
       }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
     default:
       return state;
   }
 };
+<<<<<<< HEAD
 export const isCurrentlyRecording = (state: ApplicationState): boolean => {
   return (
     state.mode === 'recording-progress' || state.mode === 'recording-paused'
   );
 };
+=======
+>>>>>>> 781c1e5 (fixes and implimented unit testing)
